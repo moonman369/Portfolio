3fca2a6d214a1cbc62d3b5521a08eb68699f40cc
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-12-28 01:17:35 +0530

    Set up 1: - Set up base directory structure - Created components for the app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Nav />
+        <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { ReactDOM } from "react";
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
