ea2619c25b79990579b62ccbbe11efd8b3ce7e39
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-12-28 12:48:47 +0530

    Set up remaining components - Created dedicated component folders - Imported them to App.jsx - Fixed main index.js code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import { ReactDOM } from "react";
+import React from "react";
+import ReactDOM  from "react-dom";
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+)
