9aac35ec387d46773223b92c06e132c851ab9189
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-12-31 01:04:09 +0530

    Working on about me section - Added styling and transition elements - Added cards info and general bio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ABT from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+      
+      <div className="container about_container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ABT} alt="About Me" />
+          </div>
+        </div>
+        
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ years Work Ex</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>300+ Clients Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Projects</small>
+            </article>
+          </div>
+
+          <p>
+          I am Ayan Maiti, a Final Year, Electrical Engineering student (Batch 2023). I have worked as a Blockchain Developer Intern at W3Dev Private Limited for four(4) months. I have an in-depth understanding of the blockchain development and its related concepts.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
