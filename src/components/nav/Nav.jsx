1b6825e1f73ace91a33f150903cc5749d8026e27
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-12-31 00:06:29 +0530

    Completed floating Nav - Added focus change and highlight using useState

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './nav.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><AiOutlineHome/></a>
-      <a href='#about'><AiOutlineUser/></a>
-      <a href='#experience'><BiBook/></a>
-      <a href='#services'><RiServiceLine/></a>
-      <a href='#contact'><BiMessageSquareDetail/></a>
+      <a href='#' onClick={() => {setActiveNav('#')}} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => {setActiveNav('#about')}} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={() => {setActiveNav('#experience')}} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href='#services' onClick={() => {setActiveNav('#services')}} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => {setActiveNav('#contact')}} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
