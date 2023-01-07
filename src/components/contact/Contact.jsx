3b499130a9bdd2edbb388d31e7d5451902b2fa0f
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-07 15:48:49 +0530

    Added contact info and links - Styling and form pending

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {FaTelegramPlane} from 'react-icons/fa'
+import {FaWhatsapp} from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMail/>
+            <h4>Email</h4>
+            <h5>mightyayan369@gmail.com</h5>
+            <a href="mailto:mightyayan369@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <FaTelegramPlane/>
+            <h4>Telegram</h4>
+            <h5>@moonman369</h5>
+            <a href="https://t.me/moonman369" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <FaWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5>+91 98302 25282</h5>
+            <a href="https://api.whatsapp.com/send?phone=919830225282" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
