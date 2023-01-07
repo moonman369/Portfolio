c984ea67ebfc71b2460e93ba8d5efad3219eecc9
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-07 12:42:38 +0530

    Completed servive component - Content - Styling

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,106 @@
 import React from 'react'
+import './services.css';
+import {GoCheck} from 'react-icons/go'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB D */}
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web3 and Blockchain</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <GoCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB3 */}
+
+      </div>
+    </section>
   )
 }
 
