30a97a82092e0ba27908c0f1278dffd0d7a3bd2e
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-07 16:07:49 +0530

    Added portfoilio icon and navigation to nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './nav.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
-import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {BiBook, BiMessageSquareDetail, BiCodeAlt} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 
 const Nav = () => {
@@ -12,6 +12,7 @@ const Nav = () => {
       <a href='#about'title='About'  onClick={() => {setActiveNav('#about')}} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience' title='Experience' onClick={() => {setActiveNav('#experience')}} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href='#services' title='Services' onClick={() => {setActiveNav('#services')}} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href='#portfolio' title='Portfolio' onClick={() => {setActiveNav('#portfolio')}} className={activeNav === '#portfolio' ? 'active' : ''}><BiCodeAlt /></a>
       <a href='#contact' title='Contact' onClick={() => {setActiveNav('#contact')}} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail name='Contact'/></a>
     </nav>
   )
