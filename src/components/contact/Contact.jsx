bb15e3549e0ab4c808a20b3af8e09f2bdd8165a7
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-01-08 19:05:15 +0530

    Completed Contact Component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,40 +1,59 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from 'emailjs-com'
 import './contact.css'
-import {AiOutlineMail} from 'react-icons/ai'
+import {TfiEmail} from 'react-icons/tfi'
 import {FaTelegramPlane} from 'react-icons/fa'
 import {FaWhatsapp} from 'react-icons/fa'
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_m61baic', 'template_9nxzoai', form.current, 'yrdLNwbctkqA0TVRm')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
-      <div className="container contact_container">
+      <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <AiOutlineMail/>
+            <TfiEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>mightyayan369@gmail.com</h5>
             <a href="mailto:mightyayan369@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <FaTelegramPlane/>
+            <FaTelegramPlane className='contact__option-icon'/>
             <h4>Telegram</h4>
             <h5>@moonman369</h5>
             <a href="https://t.me/moonman369" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <FaWhatsapp/>
+            <FaWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+91 98302 25282</h5>
             <a href="https://api.whatsapp.com/send?phone=919830225282" target='_blank'>Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" placeholder='Your Message' rows="7"></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
