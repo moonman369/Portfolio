da4408be29f31a4eb06288d4808a332acc3a3df6
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-08-11 11:49:19 +0530

    Improve matrix animation

--- src/components/matrix-rain/MatrixRain.jsx
+++ src/components/matrix-rain/MatrixRain.jsx
@@ -60,7 +60,7 @@ const RainStream = (props) => {
       setStream([]);
       const newStream = getRandStream();
       setStream(newStream);
-      setTopPadding(newStream.length * -50);
+      setTopPadding(newStream.length * -30);
       setIntervalDelay(null);
       setTimeout(
         () =>
